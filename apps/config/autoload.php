24f3ca495f61ccde178802c3e274f472661c62c3
eakkabin <eakkabin_it@outlook.co.th>
2015-03-15 12:51:29 +0700

    Update 2015-03-15-12-51

--- apps/config/autoload.php
+++ apps/config/autoload.php
@@ -2,7 +2,7 @@
 
 return array(
     ROOT_PATH . '/apps/components',
-    ROOT_PATH . '/apps/librarys',
+    ROOT_PATH . '/apps/libraries',
     ROOT_PATH . '/apps/plugins',
     ROOT_PATH . '/apps/controllers',
     ROOT_PATH . '/apps/models',
