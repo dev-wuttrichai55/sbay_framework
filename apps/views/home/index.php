2415fb7ce08e9eddb99d06e128f4e8d4f67a876d
eakkabin <eakkabin_it@outlook.co.th>
2015-03-15 20:58:56 +0700

    update logo sbay

--- apps/views/home/index.php
+++ apps/views/home/index.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-lg-12 text-center" style="padding-top:100px;">
-        <img src="<?php echo $this->baseUrl;?>/public/images/sbay.jpg" style="width: 35%;padding-bottom: 20px;">
+        <img src="<?php echo $this->baseUrl;?>/public/images/sbay-450.jpg" style="padding-bottom: 20px;">
         <h1>Welcome to <?php echo $this->pageTitle;?> version <?php echo $this->version;?></h1>
         <h1><small><?php echo $this->pageDescription;?></small></h1>
     </div>
